7d67f98c9b18dd8938c0def79aebef175d6eb805
Swayam Raina <swayamraina@gmail.com>
2018-07-22 01:08:04 +0530

    Added basic server config placeholders

---
+++ config.py
@@ -0,0 +1,21 @@
+# @author  Swayam Raina
+
+'''
+    These hold server directory mappings.
+    NOTE : Add new server instances here
+'''
+
+INSTANCE_1 = ''
+INSTANCE_2 = ''
+INSTANCE_3 = ''
+INSTANCE_4 = ''
+
+INSTANCE_1_DIR = ''
+INSTANCE_2_DIR = ''
+INSTANCE_3_DIR = ''
+INSTANCE_4_DIR = ''
+
+INSTANCE_1_LOG_DIR = ''
+INSTANCE_2_LOG_DIR = ''
+INSTANCE_3_LOG_DIR = ''
+INSTANCE_4_LOG_DIR = ''
