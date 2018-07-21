fe78d666aa0af08aa1209b4aee98feb058b32ad3
Swayam Raina <swayamraina@gmail.com>
2018-07-22 00:12:17 +0530

    Defining commands to be incorporated in airflow

---
+++ commands.py
@@ -0,0 +1,24 @@
+# @author  Swayam Raina
+
+'''
+    Set of basic commands incorporated into airflow.
+'''
+
+SERVER_START = 'start'
+SERVER_STOP = 'stop'
+SERVER_RESTART = 'restart'
+GIT_INFO = 'git'
+SERVER_HEARTBEAT = 'heartbeat'
+SERVER_LOGS = 'log'
+
+
+'''
+    Aliases for respective commands.
+'''
+
+ALIAS_SERVER_START = 's'
+ALIAS_SERVER_STOP = 'x'
+ALIAS_SERVER_RESTART = 'r'
+ALIAS_GIT_INFO = 'g'
+ALIAS_SERVER_HEARTBEAT = 'hb'
+ALIAS_SERVER_LOGS = 'l'
