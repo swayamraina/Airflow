d2ca9044cc2a69b52c44cb1e7b7c25d3c656a1a0
Swayam Raina <swayamraina@gmail.com>
2018-07-22 00:12:57 +0530

    Added handler interfaces

---
+++ handlers.py
@@ -0,0 +1,25 @@
+# @author  Swayam Raina
+
+import argparse
+
+def heartbeat_handler(args: argparse.Namespace):
+    pass
+
+def start_handler(args: argparse.Namespace):
+    pass
+
+def stop_handler(args: argparse.Namespace):
+    pass
+
+def restart_handler(args: argparse.Namespace):
+    pass
+
+def git_handler(args: argparse.Namespace):
+    pass
+
+def logs_handler(args: argparse.Namespace):
+    pass
+
+
+if __name__ == '__main__':
+    print()
