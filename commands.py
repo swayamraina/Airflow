dc2f2f9ef59051b147f11dbd66d59845886522e5
Swayam Raina <swayamraina@gmail.com>
2018-07-22 11:11:22 +0530

    Updated aliases for commands

--- commands.py
+++ commands.py
@@ -1,13 +1,17 @@
+
+# This file contains all the 'airflow' commands and 
+# their aliases.
+#
 # @author  Swayam Raina
+
 
 '''
     Set of basic commands incorporated into airflow.
 '''
-
 SERVER_START = 'start'
 SERVER_STOP = 'stop'
 SERVER_RESTART = 'restart'
-GIT_INFO = 'git'
+GIT_INFO = 'git-info'
 SERVER_HEARTBEAT = 'heartbeat'
 SERVER_LOGS = 'log'
 DEPLOY = 'deploy'
@@ -16,11 +20,10 @@ DEPLOY = 'deploy'
 '''
     Aliases for respective commands.
 '''
-
-ALIAS_SERVER_START = 's'
-ALIAS_SERVER_STOP = 'x'
-ALIAS_SERVER_RESTART = 'r'
-ALIAS_GIT_INFO = 'g'
-ALIAS_SERVER_HEARTBEAT = 'hb'
-ALIAS_SERVER_LOGS = 'l'
-ALIAS_DEPLOY = 'd'
+ALIAS_SERVER_START = '-s'
+ALIAS_SERVER_STOP = '-x'
+ALIAS_SERVER_RESTART = '-r'
+ALIAS_GIT_INFO = '-g'
+ALIAS_SERVER_HEARTBEAT = '-hb'
+ALIAS_SERVER_LOGS = '-l'
+ALIAS_DEPLOY = '-d'
