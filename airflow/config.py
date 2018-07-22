8753f176bd0e71382cb1b8c703790a0620bb6b63
Swayam Raina <swayamraina@gmail.com>
2018-07-22 12:45:41 +0530

    updated config

--- airflow/config.py
+++ airflow/config.py
@@ -9,17 +9,17 @@
     These hold server directory mappings.
     NOTE : Add new server instances here
 '''
-INSTANCE_1 = 'local'
+INSTANCE_1 = ''
 INSTANCE_2 = ''
 INSTANCE_3 = ''
 INSTANCE_4 = ''
 
-INSTANCE_1_DIR = '/Users/b0205106/projects/portal-bheem'
+INSTANCE_1_DIR = ''
 INSTANCE_2_DIR = ''
 INSTANCE_3_DIR = ''
 INSTANCE_4_DIR = ''
 
-INSTANCE_1_LOG_DIR = '/data/logs/portallog.log'
+INSTANCE_1_LOG_DIR = ''
 INSTANCE_2_LOG_DIR = ''
 INSTANCE_3_LOG_DIR = ''
 INSTANCE_4_LOG_DIR = ''
