11eeab14c034553aa47b1217ad9190aa9dae345f
Swayam Raina <swayamraina@gmail.com>
2018-07-27 23:58:15 +0530

    Added method ducumentation

--- airflow/os_utils.py
+++ airflow/os_utils.py
@@ -44,6 +44,11 @@ from os_commands import (
 )
 
 
+
+'''
+    OS utility method to fetch the server
+    directory path giver server name.
+'''
 def get_instance_dir(instance):
     index = 0
     for temp_instance in INSTANCES:
@@ -52,12 +57,22 @@ def get_instance_dir(instance):
         index = index+1
     raise ValueError('Incorrect instance name specified!')
 
+
+'''
+    OS utility method that reads the git branch
+    information.
+'''
 def get_git_branch_name(path):
     head = open(path, 'r')
     content = head.read()
     index = content.rfind(FORWARD_SLASH)
     return extract_branch_name(content, index)
 
+
+'''
+    OS utility method that reads the git branch
+    owner information.
+'''
 def get_git_branch_owner(path):
     last_log_content = subprocess.check_output(['tail', '-1', path])
     content = last_log_content.decode( UTF8 )
@@ -65,6 +80,11 @@ def get_git_branch_owner(path):
     end_index = content.find( ANGULAR_END )
     return content[start_index : end_index+1]
 
+
+'''
+    OS utility method to fetch the port on 
+    which the server is running.
+'''
 def get_instance_port(path):
     app = open(path, 'r')
     for line in app:
@@ -74,6 +94,11 @@ def get_instance_port(path):
             return extract_port_from_file(line, index)
     raise ValueError('Unable to get server port!')
 
+
+'''
+    OS utility to fetch Process ID of the application
+    given port on which it is running.
+'''
 def get_pid_from_port(port):
     query = get_system_pid_query(port)
     # special handling for 'pipe' operation
@@ -83,9 +108,18 @@ def get_pid_from_port(port):
     pid = extract_port_from_query_result(content)
     return pid
 
+
+'''
+    OS utility to stop a server instance.
+'''
 def stop_server(pid):
     subprocess.check_output(['kill', '-9', pid])
 
+
+'''
+    OS utility method to fetch the server
+    log directory path giver server name.
+'''
 def get_instance_log_dir(instance):
     index = 0
     for temp_instance in INSTANCES:
@@ -94,14 +128,28 @@ def get_instance_log_dir(instance):
         index = index + 1
     raise ValueError('Incorrect instance name specified!')
 
+
+'''
+    OS utility to display server logs as 
+    console appender.
+'''
 def display_logs(path):
     os.system(PRINT_LOGS.format(path))
 
+
+'''
+    OS utility to start a server instance.
+'''
 def start_server(path):
     subprocess.check_output(['nohup', 'gradle', '-p', path, '-q', GRADLE_TASK, 
                              '-Denv={0}'.format( ENVIRONMENT ), 
                              '-Djmxhost={0}'.format( MACHINE_IP ), '&'])
 
+
+'''
+    OS utility to actually check if the server
+    is 'up' or not.
+'''
 def pulse_check(port):
     try:
         request_url = 'http//{0}:{1}/{2}'.format(MACHINE_IP, port, HEARTBEAT_API)
@@ -115,11 +163,19 @@ def pulse_check(port):
     except Exception as e:
         print(e)
 
+
+'''
+    OS utility method for `git stash` operation.
+'''
 def stash_changes(path, display_logs):
     GIT_STASH[2] = GIT_STASH[2].format(path)
     out = subprocess.check_output(GIT_STASH)
     if display_logs: print(out.decode(UTF8))
 
+
+'''
+    OS utility method for `git checkout` operation.
+'''
 def checkout_branch(path, branch):
     success = False
     GIT_CHECKOUT[2] = GIT_CHECKOUT[2].format(path)
@@ -128,10 +184,14 @@ def checkout_branch(path, branch):
         subprocess.check_output(GIT_CHECKOUT)
         success = True
     except Exception:
-        print('No such branch exists. \nPlease check the branch name!\n')
+        print('No such branch exists.\nPlease check the branch name!\n')
         apply_stash(path, False)
     return success
 
+
+'''
+    OS utility method for `git stash pop` operation.
+'''
 def apply_stash(path, display_logs):
     GIT_POP[2] = GIT_POP[2].format(path)
     try:
@@ -141,6 +201,11 @@ def apply_stash(path, display_logs):
         print('Oops! Something went wrong. Please run the below query on project directory')
         print('git stash pop')
 
+
+'''
+    OS utility to deploy a specific branch
+    on requested server.
+'''
 def deploy_branch(path, branch):
     stash_changes(path, True)
     success = checkout_branch(path, branch)
--- airflow/utils.py
+++ airflow/utils.py
@@ -23,21 +23,45 @@ UTF8 = 'utf-8'
 EQUALS = '='
 SPACE = ' '
 
+
+'''
+    Utility method to extract branch name from
+    gi tcontent.
+'''
 def extract_branch_name(content, index):
     if content[-1] == NEWLINE:
         return content[index+1 : -1]
     return content[index+1 : ]
 
+
+'''
+    Utility method to extract server port from 
+    properties file.
+'''
 def extract_port_from_file(key_value_property, index):
     return key_value_property[ index+1 : ].strip()
 
+
+'''
+    OS level utility method that extracts the OS name.
+'''
 def get_machine_details():
     encoded_name = subprocess.check_output('uname')
     return encoded_name.decode( UTF8 )
 
+
+'''
+    Utility method that checks if the current machine
+    is a linux machine or not.
+'''
 def is_linux_machine(name):
     return name == 'Linux'
 
+
+'''
+    Utility method that returns the command to fetch 
+    the process ID irrespective of the OS.
+'''
 def get_system_pid_query(port):
     machine_type = get_machine_details()
     if is_linux_machine(machine_type):
@@ -45,6 +69,10 @@ def get_system_pid_query(port):
     MAC_PID_SEARCH_QUERY[2] = MAC_PID_SEARCH_QUERY[2].format(port)
     return MAC_PID_SEARCH_QUERY
 
+
+'''
+    Utility method to extract PID from port info.
+'''
 def extract_port_from_query_result(content):
     machine_type = get_machine_details()
     start_index = 0
@@ -62,9 +90,18 @@ def extract_port_from_query_result(content):
     end_index = content.find( SPACE )
     return content[ : end_index]
 
+
+'''
+    OS utility method that checks the python environment.
+'''
 def is_python_2():
     return sys.version_info[0] == 2
 
+
+'''
+    Utility method that creates sub parsers as per 
+    python environment.
+'''
 def create_parser(sub_parser, command, alias):
     if is_python_2():
         return sub_parser.add_parser(command)
